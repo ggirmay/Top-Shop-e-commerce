0b02475c367e709470ebd4e7c41442e4827796be
unknown <isabanej@gmail.com>
2020-07-28 16:05:42 -0500

    Financial and Reporting Module

---
+++ financialAndReporting/src/main/java/com/financialAndReporting/financialAndReporting/FinancialAndReportingApplication.java
@@ -0,0 +1,15 @@
+package com.financialAndReporting.financialAndReporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FinancialAndReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAndReportingApplication.class, args);
+	}
+
+}
---
+++ financialAndReporting/src/test/java/com/financialAndReporting/financialAndReporting/FinancialAndReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialAndReporting.financialAndReporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAndReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
