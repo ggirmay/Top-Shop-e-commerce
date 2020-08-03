316a00f8916e14140960cbbef4488bb0164ed05e
Emino <isabanej@gmail.com>
2020-08-03 09:51:30 -0500

    Reporting Service - Mid Term

---
+++ financialAndReporting/src/main/java/com/financialAndReporting/models/GeneratedReport.java
@@ -0,0 +1,16 @@
+package com.financialAndReporting.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//@Entity
+public class GeneratedReport {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+    private Order order;
+    private Product product;
+}
---
+++ financialAndReporting/src/main/java/com/financialAndReporting/models/Order.java
@@ -0,0 +1,47 @@
+package com.financialAndReporting.models;
+
+import javax.persistence.Embeddable;
+import java.time.LocalDate;
+
+@Embeddable
+public class Order {
+private Long orderId;
+private LocalDate dateCreated;
+private String userId;
+private String status;
+
+    public Order() {
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
