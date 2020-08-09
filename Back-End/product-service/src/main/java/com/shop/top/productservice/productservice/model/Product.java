0e45cf172d56a147350f8fa7b30456def29f1a0a
Mulieman <57609006+Mulieman@users.noreply.github.com>
2020-08-09 16:43:36 -0500

    upload image

--- Back-End/product-service/src/main/java/com/shop/top/productservice/productservice/model/Product.java
+++ Back-End/product-service/src/main/java/com/shop/top/productservice/productservice/model/Product.java
@@ -5,7 +5,10 @@ import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
@@ -30,7 +33,21 @@ public class Product implements Serializable {
     @ManyToOne
     private Category category;
 
-@OneToOne
+    @OneToOne
     private ProductDetail productDetail;
 
+    private double salesPrice;
+
+    private double discount;
+    private String shortDetails;
+    private String description;
+    private boolean newPro;
+    private boolean sale;
+    private String brand;
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<Picture> pictures;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tag",
+            joinColumns = @JoinColumn(name="product_id"))
+    private Set<String> tags = new HashSet<String>();
 }
