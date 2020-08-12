bbb42e3627ff8f7f6688cb279e9e5d755d2bc990
Emino <isabanej@gmail.com>
2020-08-12 15:48:42 -0500

    Sales - Report

---
+++ Back-End/shoppingCart/src/main/java/com/shop/top/shoppingcart/models/OrderDetailList.java
@@ -0,0 +1,15 @@
+package com.shop.top.shoppingcart.models;
+
+import java.util.List;
+
+public class OrderDetailList {
+    List<OrderDetail> orderDetails;
+
+    public List<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+}
