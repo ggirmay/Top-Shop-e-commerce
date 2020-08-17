0d7d74ac15896a5784e8df56b3991aed86c420fd
Ny ANDRIANTSOA <nyandriantsoa25@gmail.com>
2020-08-17 04:40:29 -0500

    back end payment gateway

--- Back-End/payment/src/main/java/com/shop/top/payment/payment/config/JavaMailConfiguration.java
+++ Back-End/payment/src/main/java/com/shop/top/payment/payment/config/JavaMailConfiguration.java
@@ -11,36 +11,35 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
-@Configuration
 public class JavaMailConfiguration {
 
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("socialnetworks565@gmail.com");
-        mailSender.setPassword("I1234567890mnbvcxz");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
-    @Autowired
-    private JavaMailSender mailSender;
-
-    public void send(){
-
-
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setTo("my email");
-        email.setSubject("my subject");
-        email.setText( "balh blah ");
-        mailSender.send(email);
-    }
+//    @Bean
+//    public JavaMailSender getJavaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//
+//        mailSender.setUsername("socialnetworks565@gmail.com");
+//        mailSender.setPassword("I1234567890mnbvcxz");
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//
+//        return mailSender;
+//    }
+//    @Autowired
+//    private JavaMailSender mailSender;
+//
+//    public void send(){
+//
+//
+//        SimpleMailMessage email = new SimpleMailMessage();
+//        email.setTo("my email");
+//        email.setSubject("my subject");
+//        email.setText( "balh blah ");
+//        mailSender.send(email);
+//    }
 }
